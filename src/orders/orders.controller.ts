15977fd437a7189b9f9d956f0a11c9d568245dc9
JHOEL ANDRES ZEBALLOS ROMERO <teoremaff377@gmail.com>
2026-04-24 08:08:07 -0400

    fix: tipar req.user en orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -11,6 +11,11 @@ import {
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/order.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { User } from '../users/entities/user.entity';
+
+interface AuthRequest extends Request {
+  user: User;
+}
 
 @Controller('orders')
 @UseGuards(JwtAuthGuard)
@@ -18,17 +23,17 @@ export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto, @Request() req) {
+  create(@Body() createOrderDto: CreateOrderDto, @Request() req: AuthRequest) {
     return this.ordersService.create(createOrderDto, req.user);
   }
 
   @Get('my-orders')
-  findMyOrders(@Request() req) {
+  findMyOrders(@Request() req: AuthRequest) {
     return this.ordersService.findMyOrders(req.user);
   }
 
   @Patch(':id/cancel')
-  cancelOrder(@Param('id') id: string, @Request() req) {
+  cancelOrder(@Param('id') id: string, @Request() req: AuthRequest) {
     return this.ordersService.cancelOrder(id, req.user);
   }
 }
