406139a773c0896978feb33e88ab43b05da02a70
Edmilzon <edmilzon.luna@gmail.com>
2026-05-20 23:04:19 -0400

    fix: delete

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,23 +11,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.init();
-  return server;
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
-
-if (!process.env.VERCEL) {
-  bootstrap().then((app) =>
-    app.listen({ port: process.env.PORT ?? 3000, host: '0.0.0.0' }),
-  );
-}
-
-let cachedApp: express.Express;
-export default async function handler(
-  req: express.Request,
-  res: express.Response,
-) {
-  if (!cachedApp) {
-    cachedApp = await bootstrap();
-  }
-  cachedApp(req, res);
-}
+bootstrap();
