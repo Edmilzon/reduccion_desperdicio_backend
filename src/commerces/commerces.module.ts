f41b93de2f6b5dd7de7ef0896cf94aa4197920cc
guimeloa <guim.cc.17@gmail.com>
2026-05-28 17:48:47 -0400

    BACK-01/02/03: agregar filtro category y radius en /commerces/nearby, endpoints /restaurants/nearby y /restaurants/categories

--- src/commerces/commerces.module.ts
+++ src/commerces/commerces.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Commerce } from './entities/commerce.entity';
 import { Location } from './entities/location.entity';
 import { Product } from '../products/entities/product.entity';
+import { Category } from '../products/entities/category.entity';
 import { CommercesService } from './commerces.service';
 import { CommercesController } from './commerces.controller';
 import { RestaurantsController } from './restaurants.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Commerce, Location, Product])],
+  imports: [TypeOrmModule.forFeature([Commerce, Location, Product, Category])],
   controllers: [CommercesController, RestaurantsController],
   providers: [CommercesService],
   exports: [TypeOrmModule, CommercesService],
--- src/commerces/restaurants.controller.ts
+++ src/commerces/restaurants.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query, BadRequestException } from '@nestjs/common';
 import { CommercesService } from './commerces.service';
 
 @Controller('restaurants')
 export class RestaurantsController {
   constructor(private readonly commercesService: CommercesService) {}
+
+  @Get('nearby')
+  findNearby(
+    @Query('lat') lat: string,
+    @Query('lng') lng: string,
+    @Query('radius') radius: string,
+    @Query('category') category?: string,
+  ) {
+    if (!lat || !lng) {
+      throw new BadRequestException(
+        'lat and lng query parameters are required',
+      );
+    }
+    const radiusKm = radius ? parseFloat(radius) : 0;
+    return this.commercesService.findByCoordinates(
+      parseFloat(lat),
+      parseFloat(lng),
+      radiusKm,
+      category,
+    );
+  }
+
+  @Get('categories')
+  findActiveCategories() {
+    return this.commercesService.findActiveCategories();
+  }
 
   @Get(':id/detail')
   getDetail(@Param('id') id: string) {
